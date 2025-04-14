b2eeda5b841e85d550b301632007e2b652456f22
jakobes <me@jakobes.net>
2025-04-15 00:02:47 +0200

    more work onday3

--- 2024/aoc/day3/src/main.rs
+++ 2024/aoc/day3/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use regex::Regex;
 use std::fs;
 
@@ -6,33 +6,73 @@ fn read_file(filename: &str) -> Result<String> {
     fs::read_to_string(filename).with_context(|| format!("Failed to read file: {filename}"))
 }
 
-fn extract_and_calculate(input: &str) -> Result<Vec<u32>> {
-    let pattern = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do(n't)?\(\))";
+fn part1(input: &str) -> Result<Vec<u32>> {
+    let pattern = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)";
     let regex = Regex::new(pattern).context("Failed to compile regex pattern")?;
-
     let mut results = Vec::new();
 
     for cap in regex.captures_iter(input) {
         let x = cap[1]
             .parse::<u32>()
-            .with_context(|| format!("Failed to [parse number."))?;
+            .with_context(|| format!("Failed to parse number."))?;
         let y = cap[2]
             .parse::<u32>()
-            .with_context(|| format!("Failed to [parse number."))?;
+            .with_context(|| format!("Failed to parse number."))?;
         results.push(x * y);
     }
 
     Ok(results)
 }
 
+fn part2(input: &str) -> Result<Vec<u32>> {
+    let pattern = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do(n't)?\(\))";
+    let regex = Regex::new(pattern).context("Failed to compile regex pattern")?;
+
+    let mut results = Vec::new();
+    let mut include = true;
+
+    for caps in regex.captures_iter(input) {
+        match (caps.get(1), caps.get(2), caps.get(3)) {
+            (Some(a), Some(b), _) if include => {
+                let x = a
+                    .as_str()
+                    .parse::<u32>()
+                    .context("Failed to parse number")?;
+                let y = b
+                    .as_str()
+                    .parse::<u32>()
+                    .context("Failed to parse number")?;
+                results.push(x * y);
+            }
+            (_, _, Some(do_call)) => match do_call.as_str() {
+                "do()" => include = true,
+                "don't()" => include = false,
+                other => return Err(anyhow!("Unexpected do-call: {}", other)),
+            },
+            _ => return Err(anyhow!("Unexpected match structure")),
+        }
+    }
+
+    Ok(results)
+}
+
 fn main() -> Result<()> {
-    // let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     let input = read_file("input_data/input.txt")?;
 
-    match extract_and_calculate(input.as_str()) {
+    // match part1(input.as_str()) {
+    //     Ok(results) => {
+    //         println!("Total sum: {}", results.iter().sum::<u32>());
+    //         Ok(())
+    //     }
+    //     Err(err) => {
+    //         eprintln!("Error: {}", err);
+    //         Err(err)
+    //     }
+    // }
+
+    match part2(input.as_str()) {
         Ok(results) => {
-            println!("Individual results: {:?}", results);
-            println!("Total sum: {}", results.iter().sum::<u32>());
+            println!("Ok!");
             Ok(())
         }
         Err(err) => {
